32bb09ebb59611cc1824fbcdb70aeb62a38874cf
egonl <egon.lubbers@gmail.com>
2023-09-20 22:36:46 +0200

    Added rfNavbar.css

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,10 +1,11 @@
+import './rfNavbar.css';
 import React from 'react';
 import Navbar from '@theme-original/Navbar';
 
 export default function NavbarWrapper(props) {
   return (
     <>
-      <div className='rfNavbar'>
+      <div className='rfNavbar navbar--fixed-top'>
         <Navbar {...props} />
       </div>
     </>
