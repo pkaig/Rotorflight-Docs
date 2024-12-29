607f993bdfa9dde2cc18b864550df088080f2f04
Phil Kaighin <pkaig@westnet.com.au>
2024-12-29 12:00:54 +1000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
             if (existingPath.includes('/test')) {
               // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
               return [
-                existingPath.replace('/docs/test', '/docs/next'),
+                existingPath.replace('/docs/next', '/docs/test'),
               ];
             }
             return undefined; // Return a falsy value: no redirect created
